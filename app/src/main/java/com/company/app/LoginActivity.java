95092b1294120cd219335f27bd5dcb1ec27f9a7d
hector <hector@hector.hector>
2017-06-13 09:50:59 -0500

    App avance Home activity

--- app/src/main/java/com/company/app/LoginActivity.java
+++ app/src/main/java/com/company/app/LoginActivity.java
@@ -1,7 +1,12 @@
 package com.company.app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.company.app.view.ContainerActivity;
+import com.company.app.view.CreateAccountActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +14,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void goCreateAccount(View view){
+        Intent intent = new Intent(this, CreateAccountActivity.class);
+        startActivity(intent);
+    }
+    public void goContainer(View view){
+        Intent intent = new Intent(this, ContainerActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/company/app/view/CreateAccountActivity.java
+++ app/src/main/java/com/company/app/view/CreateAccountActivity.java
@@ -2,6 +2,7 @@ package com.company.app.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.company.app.R;
 
@@ -11,6 +12,14 @@ public class CreateAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        showToolbar(getResources().getString(R.string.toolbar_title_createacount), true);
     }
 
+    public void showToolbar(String tittle, boolean upButton){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(tittle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+    }
 }
